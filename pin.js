68c17aed7ce2ecb50ff95f209d4dadd0fd916a46
[._.]/ Adam Eivy <70015+atomantic@users.noreply.github.com>
2021-03-23 10:10:30 -0700

    adding ipfs pin tool

---
+++ pin.js
@@ -0,0 +1,28 @@
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+const conseilUtil = require('./conseilUtil');
+
+const pin = async(hash)=>{
+    const { stdout, stderr } = await exec(`ipfs pin add ${hash}`);
+    if(stderr) console.error(`error pinnng ${hash}`, stderr);
+    console.log(stdout);
+}
+
+const tz = process.argv[2];
+
+console.log(`fetching content for address ${tz}`);
+
+(async () => {
+
+    const collection = await conseilUtil.getCollectionForAddress(tz)
+    const creations = await conseilUtil.getArtisticOutputForAddress(tz)
+
+    const hashes = [...collection.map(i=>i.ipfsHash), ...creations.map(i=>i.ipfsHash)];
+
+    console.log(`hashes`, hashes);
+
+    for(let i=0;i<hashes.length;i++){
+        await pin(hashes[i]);
+    }
+
+})();
